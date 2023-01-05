3cf94993a6a17f658c337b89ea280638e6cdd6bb
Hidey Boi <maxg2070319@gmail.com>
2023-01-04 23:53:49 -0600

    rooms

--- Assets/Scripts/DungeonGen/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGen/DungeonGenerator.cs
@@ -32,33 +32,10 @@ public class DungeonGenerator : MonoBehaviour
         public bool[] status = new bool[4];
     }
 
-    [System.Serializable]
-    public class Rule
-    {
-        public GameObject room;
-        public Vector2Int minPosition;
-        public Vector2Int maxPosition;
-
-        public bool obligatory;
-
-        public int ProbabilityOfSpawning(int x, int y)
-        {
-            // 0 - cannot spawn 1 - can spawn 2 - HAS to spawn
-
-            if (x>= minPosition.x && x<=maxPosition.x && y >= minPosition.y && y <= maxPosition.y)
-            {
-                return obligatory ? 2 : 1;
-            }
-
-            return 0;
-        }
-
-    }
-
     public Vector2Int size;
     public int startPos = 0;
     public GameObject baseRoom;
-    public Rule[] rooms;
+    public GameObject[] rooms;
     public Vector2 offset;
 
     List<Cell> board;
@@ -129,7 +106,7 @@ public class DungeonGenerator : MonoBehaviour
         Quaternion rot = msg.GetQuaternion();
 
         var spawnedBase = Instantiate(instance.baseRoom, roomPos, Quaternion.identity, instance.transform);
-        Instantiate(instance.rooms[roomId].room, spawnedBase.transform.position, rot, spawnedBase.transform);
+        Instantiate(instance.rooms[roomId], spawnedBase.transform.position, rot, spawnedBase.transform);
 
         
 
@@ -159,34 +136,9 @@ public class DungeonGenerator : MonoBehaviour
                 Cell currentCell = board[(i + j * size.x)];
                 if (currentCell.visited)
                 {
-                    int randomRoom = -1;
-                    List<int> availableRooms = new List<int>();
+                    int randomRoom = 0;
 
-                    for (int k = 0; k < rooms.Length; k++)
-                    {
-                        int p = rooms[k].ProbabilityOfSpawning(i, j);
-
-                        if(p == 2)
-                        {
-                            randomRoom = k;
-                            break;
-                        } else if (p == 1)
-                        {
-                            availableRooms.Add(k);
-                        }
-                    }
-
-                    if(randomRoom == -1)
-                    {
-                        if (availableRooms.Count > 0)
-                        {
-                            randomRoom = availableRooms[Random.Range(0, rooms.Length)];
-                        }
-                        else
-                        {
-                            randomRoom = 0;
-                        }
-                    }
+                    randomRoom = Random.Range(0, rooms.Length);
 
                     Quaternion rotation = Quaternion.identity;
 
@@ -209,7 +161,7 @@ public class DungeonGenerator : MonoBehaviour
                     }
 
                     var spawnedBase = Instantiate(baseRoom, new Vector3(i * offset.x, 0, -j * offset.y), Quaternion.identity);
-                    Instantiate(rooms[randomRoom].room, spawnedBase.transform.position, rotation, spawnedBase.transform);
+                    Instantiate(rooms[randomRoom], spawnedBase.transform.position, rotation, spawnedBase.transform);
 
                     var newRoom = spawnedBase.GetComponent<RoomBehaviour>();
                     newRoom.UpdateRoom(currentCell.status);
